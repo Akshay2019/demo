dcce35d3ed3a9dd1fbda553a3111608ab74adce1
MANSI <MANSI@192.168.2.2>
2019-01-08 21:30:51 +0530

    master project

--- Demo/src/com/hd/TestClass1.java
+++ Demo/src/com/hd/TestClass1.java
@@ -7,31 +7,57 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.rmi.CORBA.Util;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.Select;
 //import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestClass1 extends readprop {
 
-	public static void main(String[] args) throws FileNotFoundException  {
+	public static void main(String[] args) throws IOException  {
 
-	// System.setProperty("webdriver.ie.driver",
-	//			 "C:\\Users\\MANSI\\Downloads\\Automation\\IEDriverServer_x64_3.13.0\\IEDriverServer.exe");
+	 System.setProperty("webdriver.ie.driver",
+				 "C:\\Users\\MANSI\\Downloads\\Automation\\IEDriverServer_x64_3.13.0\\IEDriverServer.exe");
 
-//	System.setProperty("webdriver.gecko.driver",
-//				"C:\\Users\\mansi.delhiwala\\Downloads\\geckodriver-v0.20.0-win32\\geckodriver.exe");
+	//System.setProperty("webdriver.gecko.driver",
+	//			"C:\\Users\\mansi.delhiwala\\Downloads\\geckodriver-v0.20.0-win32\\geckodriver.exe");
 
-	//	 WebDriver driver = new InternetExplorerDriver();
+	
+//	System.setProperty("webdriver.chrome.driver","C:\\Users\\MANSI\\Downloads\\Automation\\chromedriver_win32\\chromedriver.exe");
+	
+		// WebDriver driver = new InternetExplorerDriver();
 		 
 		// File src = new File("C:\\Excel\\New Microsoft Excel Worksheet.xlsx");
 		// FileInputStream fis = new  FileInputStream(src);
 		 
-		WebDriver driver = new FirefoxDriver();
+	 WebDriver driver = new InternetExplorerDriver();	
+	// WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.get("https://breeze-online-21.breeze.ca.gov?");
+//		TakesScreenshot ts = (TakesScreenshot)driver;
+//		File source = ts.getScreenshotAs(OutputType.FILE);
+//		FileHandler.copy(source,new File ("./SCREENSHOTS/1.png"));
+	//	driver.findElement(By.id("infoBlockIDImage")).click();
+	//	driver.findElement(By.id("overridelink")).click();
+		driver.findElement(By.id("userid")).sendKeys("VOUSER_ST_636");
+		driver.findElement(By.id("password")).sendKeys("Password123!");
+		driver.findElement(By.name("logon")).click();
+
+	
+	}
+
+		
+		
+		
 		//	InputStream input = new FileInputStream("/Users/mansi.delhiwala/workspace1/Demo/src/property/Login.properties");
 		//	prop.load(input);
 			
@@ -54,7 +80,7 @@ public class TestClass1 extends readprop {
 		 
 		 // driver.get(prop.getProperty("URL"));
 
-		driver.get("https://breeze-online-20.breeze.ca.gov?");
+	/*	driver.get("https://breeze-online-20.breeze.ca.gov?");
 
 	driver.findElement(By.id("infoBlockIDImage")).click();
 	driver.findElement(By.id("overridelink")).click();
@@ -130,6 +156,6 @@ public class TestClass1 extends readprop {
 	driver.findElement(By.name("next")).click();
 	
 	driver.findElement(By.name("next")).click();
+	*/
+	}
 	
-	}
-	}
